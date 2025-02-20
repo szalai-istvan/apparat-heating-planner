b0c1ea2b8b61753539892302c2cdf9fa6ed4eb72
szalai-istvan <szalai.istvan95@gmail.com>
2025-02-20 21:15:45 +0100

    excel generating continued

--- js/v2/classes/quote/excel/excelGenerator.js
+++ js/v2/classes/quote/excel/excelGenerator.js
@@ -4,7 +4,7 @@ const testSummary = '{"konyha":{"F400":{"count":7,"unitPrice":400,"price":2800},
 const testSummaryObject = JSON.parse(testSummary);
 
 let testContext;
-async function testExport() {
+function testExport() {
     createExcelFile(testSummaryObject).then(c => testContext = c);
 }
 
@@ -23,17 +23,17 @@ async function createExcelFile(summary) {
     const workbook = await loadExcelTemplate();
     context.workbook = workbook;
 
-    // const summarySheet = workbook.addWorksheet(SUMMARY_SHEET_NAME);
-    // const blueprintSheet = workbook.addWorksheet(BLUEPRINT_SHEET_NAME);
     const summarySheet = workbook.getWorksheet(SUMMARY_SHEET_NAME);
     const blueprintSheet = workbook.getWorksheet(BLUEPRINT_SHEET_NAME);
     context.sheets = {summarySheet, blueprintSheet};
 
-    const columns = 15 + roomNames.length;
-    context.columns = {summarySheet: columns};
-
-    // Kell elég oszlop
     createColumnsForRooms(context);
+    fillRoomNames(context);
+    fillPanelCounts(context);
+    fillRoomSummaryFormulas(context);
+    fillPanelCountFormulas(context);
+    fillCoolingAndHeatingFormulas(context);
+
     // add content
     // add picture
     // pack up and download
@@ -61,7 +61,7 @@ function downloadExcel(context) {
 
         const link = document.createElement("a");
         link.href = URL.createObjectURL(blob);
-        link.download = "edited_data_with_borders.xlsx";
+        link.download = `apparat_excel_${Math.random().toString().substring(2)}.xlsx`;
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -83,5 +83,109 @@ function createColumnsForRooms(context) {
             targetCell.style = { ...sourceCell.style };
             targetCell.formula = sourceCell.formula;
         });
+    }
+}
+
+function fillRoomNames(context) {
+    const roomNames = context.roomNames;
+    const summarySheet = context.sheets.summarySheet;
+    const row = summarySheet.getRow(2);
+    let column = 8;
+    
+    roomNames.forEach(roomName => row.getCell(column++).value = roomName);
+}
+
+function fillPanelCounts(context) {
+    const roomNames = context.roomNames;
+    const summarySheet = context.sheets.summarySheet;
+    const summary = context.summary;
+
+    let rowIndex = 3;
+    while (rowIndex < 10) {
+        let columnIndex = 7;
+
+        const row = summarySheet.getRow(rowIndex);
+        const panelType = row.getCell(columnIndex++).value;
+
+        for (let roomName of roomNames) {
+            const count = summary[roomName][panelType]?.count;
+            row.getCell(columnIndex++).value = count;
+        }
+        rowIndex++;
+    }
+}
+
+function fillRoomSummaryFormulas(context) {
+    const roomNames = context.roomNames;
+    const summarySheet = context.sheets.summarySheet;
+    
+    let columnIndex = 8;
+    for (let roomName of roomNames) {
+        let coolingFormula = [];
+        let heatingFormula = [];
+        let roundsFormula = [];
+        let areaFormula = [];
+
+        let rowIndex = 3;
+        while (rowIndex < 10) {
+            const cellName = getCellName({column: columnIndex, row: rowIndex});
+            coolingFormula.push(`${cellName}*${getCellName({column: 4, row: rowIndex, dollars: '$$'})}`);
+            heatingFormula.push(`${cellName}*${getCellName({column: 5, row: rowIndex, dollars: '$$'})}`);
+            roundsFormula.push(`${cellName}*${getCellName({column: 6, row: rowIndex, dollars: '$$'})}`);
+            areaFormula.push(`${cellName}*${getCellName({column: 3, row: rowIndex, dollars: '$$'})}`);
+            rowIndex++;
+        }
+        coolingFormula = coolingFormula.join('+');
+        heatingFormula = heatingFormula.join('+');
+
+        roundsFormula = roundsFormula.join('+');
+        roundsFormula = `ROUNDUP(((${roundsFormula})/130),0)`;
+        areaFormula = areaFormula.join('+');
+
+        setFormula(summarySheet.getRow(12).getCell(columnIndex), coolingFormula);
+        setFormula(summarySheet.getRow(13).getCell(columnIndex), heatingFormula);
+        setFormula(summarySheet.getRow(15).getCell(columnIndex), roundsFormula);
+        setFormula(summarySheet.getRow(16).getCell(columnIndex), areaFormula);
+        
+        columnIndex++;
+    }
+
+    const lastRoomColumn = 7 + roomNames.length;
+    setFormula(summarySheet.getRow(15).getCell(7), `SUM(${getRangeName({c1: {row: 15, column: 8}, c2: {row: 15, column: lastRoomColumn}})})`);
+    setFormula(summarySheet.getRow(16).getCell(7), `SUM(${getRangeName({c1: {row: 16, column: 8}, c2: {row: 16, column: lastRoomColumn}})})`);
+
+    context.lastRoomColumn = lastRoomColumn;
+}
+
+function fillPanelCountFormulas(context) {
+    const summarySheet = context.sheets.summarySheet;
+    const lastRoomColumn = context.lastRoomColumn;
+    const targetColumn = lastRoomColumn + 2;
+
+    let rowIndex = 3;
+    while (rowIndex < 10) {
+        const range = {c1: {row: rowIndex, column: 8}, c2: {row: rowIndex, column: lastRoomColumn}};
+        setFormula(summarySheet.getRow(rowIndex).getCell(targetColumn), `SUM(${getRangeName(range)})`);
+        rowIndex++;
+    }
+}
+
+function fillCoolingAndHeatingFormulas(context) {
+    const summarySheet = context.sheets.summarySheet;
+    const lastRoomColumn = context.lastRoomColumn;
+
+    const countColumn = lastRoomColumn + 2;
+    const targetColumnCooling = lastRoomColumn + 3;
+    const targetColumnHeating = targetColumnCooling + 1;
+
+    let rowIndex = 3;
+    while (rowIndex < 10) {
+        const coolingCell = {row: rowIndex, column: 4};
+        const heatingCell = {row: rowIndex, column: 5};
+        const countCell = {row: rowIndex, column: countColumn};
+        
+        setFormula(summarySheet.getRow(rowIndex).getCell(targetColumnCooling), `${getCellName(coolingCell)}*${getCellName(countCell)}`);
+        setFormula(summarySheet.getRow(rowIndex).getCell(targetColumnHeating), `${getCellName(heatingCell)}*${getCellName(countCell)}`);
+        rowIndex++;
     }
 }
--- js/v2/classes/quote/excel/excelGeneratorUtil.js
+++ js/v2/classes/quote/excel/excelGeneratorUtil.js
@@ -20,6 +20,8 @@ function getRangeName(range) {
     const column2 = range.c2.column;
     const row1 = range.c1.row;
     const row2 = range.c2.row;
+    const dollars1 = range.c1.dollars;
+    const dollars2 = range.c2.dollars;
 
     if (column1 === column2 && row1 === row2) {
         return getCellName(range.c1);
@@ -31,13 +33,22 @@ function getRangeName(range) {
 function getCellName(cell) {
     const row = cell.row;
     const column = cell.column;
+    const dollars = cell.dollars || '__';
+    const d1 = dollars[0] === '$' ? '$' : '';
+    const d2 = dollars[1] === '$' ? '$' : '';
 
     if (ALPHABET[column - 1]) {
-        return `${ALPHABET[column - 1]}${row}`;
+        return `${d1}${ALPHABET[column - 1]}${d2}${row}`;
     }
 
     const firstChar = Math.floor(column / 26);
     const secondChar = Math.floor(column % 26);
 
-    return `${ALPHABET[firstChar - 1]}${ALPHABET[secondChar - 1]}${row}`;
+    return `${d1}${ALPHABET[firstChar - 1]}${ALPHABET[secondChar - 1]}${d2}${row}`;
+}
+
+function setFormula(cell, formula) {
+    cell.formula = formula;
+    cell.formulaType = 1;
+    cell.value = {formula};
 }
