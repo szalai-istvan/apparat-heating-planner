124fdcb769c574b9cf107f2a18d08566adb0771c
istvan.szalai <istvan.szalai@icellmobilsoft.hu>
2025-12-22 21:23:45 +0100

    project merging continued

--- js/common/ui/screen/dragAndZoom.js
+++ js/common/ui/screen/dragAndZoom.js
@@ -1,21 +1,44 @@
+/** @type {boolean} */
 let screenDraggingInProgress;
+/** @type {Point} */
 let screenDragStartPosition;
+/** @type {number} */
 let screenZoom = 1;
 
+/**
+ * Elindítja a képernyő mozgatását.
+ * 
+ * @returns {undefined}
+ */
 function startDragging() {
     screenDragStartPosition = getMousePosition();
     screenDraggingInProgress = true;
 }
 
+/**
+ * Leállítja a képernyő mozgatását.
+ * 
+ * @returns {undefined}
+ */
 function stopDragging() {
     updateScreenSumDrag();
     screenDraggingInProgress = false;
 }
 
+/**
+ * Képernyő felfelé zoomolás.
+ * 
+ * @returns {undefined}
+ */
 function zoomIn() {
     screenZoom = Math.min(MAXIMUM_ZOOM, screenZoom * ZOOM_STEP);
 }
 
+/**
+ * Képernyő lefelé zoomolás.
+ * 
+ * @returns {undefined}
+ */
 function zoomOut() {
     screenZoom = Math.max(MINIMUM_ZOOM, screenZoom / ZOOM_STEP);
 }
